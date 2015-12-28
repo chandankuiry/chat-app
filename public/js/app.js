9e839ea0b73d6676389e802b20362df6770ad309
chandan kuiry <kuirychandan@gmail.com>
2015-12-28 15:52:39 +0530

    add bootstrap in chat page

--- public/js/app.js
+++ public/js/app.js
@@ -17,12 +17,14 @@ socket.on('connect', function () {
 //to listen the message what we sent
 socket.on('message', function (message) {
 	var momentTimestamp =moment.utc(message.timestamp)
-	var $message = jQuery('.messages');
+	var $messages = jQuery('.messages');
+	var $message=jQuery('<li class="list-group-item"></li>');
 	console.log('New message');
 	console.log(message.text);
 
 	$message.append('<p><strong>' + message.name + ' '+' ' +momentTimestamp.local().format('h:mm a')+ '</strong></p>');
 	$message.append('<p>'+ message.text+'</p>');
+	$messages.append($message);
 });
 //HANDLE SUBMITTING FOR NEW MESSAGE
 var $form =jQuery('#message-form');//here $ we use access jquery element
